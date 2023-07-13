4a1de287f09b93c12ce575707a822a4992bd7d20
madyanes <madyanes.dev@gmail.com>
2023-07-13 21:27:36 +0700

    membuat fungsi login logout utk ubah visibility

--- assets/app.js
+++ assets/app.js
@@ -16,6 +16,26 @@ const openLoginForm = () => {
     document.querySelector('.form-login-popup').style.display = 'block';
 }
 
+// login
+const login = () => {
+    const username = document.querySelector('#username').value
+    const password = document.querySelector('#password').value
+
+    if (username === localStorage.getItem('username') && password === localStorage.getItem('password')) {
+        document.querySelector('#login').style.display = 'none'
+        document.querySelector('#logout').style.setProperty('display', 'inline', 'important')
+
+        closeLoginForm()
+    }
+}
+
+// logout
+const logout = () => {
+    console.log('logout')
+    document.querySelector('#logout').style.display = 'none'
+    document.querySelector('#login').style.setProperty('display', 'inline', 'important')
+}
+
 
 // eksekusi fungsi di bawah setelah dokumen ditampilkan keseluruhan
 window.onload = checkOrCreateUsername()
@@ -23,3 +43,5 @@ window.onload = checkOrCreateUsername()
 // Event Listeners
 document.querySelector('#login').addEventListener('click', openLoginForm)
 document.querySelector('#btn-login-batal').addEventListener('click', closeLoginForm)
+document.querySelector('#btn-login').addEventListener('click', login)
+document.querySelector('#logout').addEventListener('click', logout)
